9ae520e738deaf7febe9c9ce27ad4805532fe21d
Ícaro Soares <icarosa21@gmail.com>
2026-06-22 14:32:27 -0300

    Atualizei o ex045

--- mundo 2/ex045.py
+++ mundo 2/ex045.py
@@ -13,7 +13,30 @@ if jogador not in [0, 1, 2]:
     print('Jogada inválida!')
     quit()
 else:
-    print(f'Você escolheu {itens[jogador]}!')
-    print(f'O computador escolheu {itens[computador]}!')
-    # condição do jogador ganhar, perder ou empatar
-    
+    print('Jo...', end='')
+    sleep(1)
+    print('Ken...', end='')
+    sleep(1)
+    print('Pô!')
+    sleep(1)
+    print('-=' * 30)
+    print(f'Você jogou {itens[jogador]}!')
+    print(f'O computador jogou {itens[computador]}')
+    print('-=' * 30)
+    if itens[jogador] == itens[computador]:
+        print('Deu empate!')
+    elif itens[jogador] == 'Pedra':
+        if itens[computador] == 'Papel':
+            print('Você perdeu!')
+        else:
+            print('Você ganhou!')
+    elif itens[jogador] == 'Papel':
+        if itens[computador] == 'Tesoura':
+            print('Você perdeu!')
+        else:
+            print('Você ganhou!')
+    elif itens[jogador] == 'Tesoura':
+        if itens[computador] == 'Pedra':
+            print('Você perdeu!')
+        else:
+            print('Você ganhou!')
