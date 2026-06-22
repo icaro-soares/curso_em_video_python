0dd03913ed69009ca1c5c7f52e1d7b55a65e8bc0
Ícaro Soares <icarosa21@gmail.com>
2026-06-22 09:13:18 -0300

    Atualizei o ex045

--- mundo 2/ex045.py
+++ mundo 2/ex045.py
@@ -3,3 +3,17 @@
 
 from random import randint
 from time import sleep
+
+
+itens = ['Pedra', 'Papel', 'Tesoura']
+# escolha do computador e do jogador
+computador = randint(0, 2)
+jogador = int(input('Sua jogada: [0 - Pedra][1 - Papel][2 - Tesoura] '))
+if jogador not in [0, 1, 2]:
+    print('Jogada inválida!')
+    quit()
+else:
+    print(f'Você escolheu {itens[jogador]}!')
+    print(f'O computador escolheu {itens[computador]}!')
+    # condição do jogador ganhar, perder ou empatar
+    
