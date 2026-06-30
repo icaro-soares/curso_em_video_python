d5d7863aca61d64a0f8e45e99627b3d25665bc7c
Ícaro Soares <icarosa21@gmail.com>
2026-06-30 09:40:47 -0300

    Atualizei o repositório

--- mundo 2/ex068.py
+++ mundo 2/ex068.py
@@ -7,3 +7,14 @@ while jogador not in range(0, 11):
     jogador = int(input('Escolha um número: [entre 0 e 10] '))
     if jogador not in range(0, 11):
         print('Válido apenas número de dedos!')
+soma = jogador + computador
+opc = ' '
+while opc not in 'PpIi':
+    opc = input('Par ou Ímpar: [P/I] ').strip()[0]
+    if opc not in 'PpIi':
+        print('Escolha inválida, escolha novamente!')
+print(f'Você jogou {jogador} e o computador {computador}.')
+if opc in 'Pp':
+    print('Par. Ganhou!') if soma % 2 == 0 else print('Ímpar. Perdeu!')
+else:
+    print('Ímpar. Ganhou!') if soma % 2 != 0 else print('Par. Perdeu!')
