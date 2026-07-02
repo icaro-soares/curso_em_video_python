f5d03ecf211b45422afcabf37a01a25683b08fa0
Ícaro Soares <icarosa21@gmail.com>
2026-07-02 09:25:33 -0300

    Atualizei o repositório

---
+++ mundo 3/ex072.py
@@ -0,0 +1,5 @@
+núm = {
+        'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte', 
+}
+n = int(input('Digite um número (0 a 20) para vê-lo escrito por extenso: '))
+print(f'Você digitou {núm[n]}')
