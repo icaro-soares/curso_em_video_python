d6e666e230352b641b889db204f15336b9063d6a
Ícaro Soares <icarosa21@gmail.com>
2026-07-03 08:57:20 -0300

    Atualizei o ex072

--- mundo 3/ex072.py
+++ mundo 3/ex072.py
@@ -1,5 +1,10 @@
-núm = {
-        'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte', 
-}
-n = int(input('Digite um número (0 a 20) para vê-lo escrito por extenso: '))
-print(f'Você digitou {núm[n]}')
+números = (
+        'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezesete', 'dezoito', 'dezenove', 'vinte'
+)
+while True:
+        n = int(input('Digite um número: [0 a 20] '))
+        if n not in range(0, 21):
+                print('Opção inválida!')
+        else:
+                break
+print(f'Você digitou o número {números[n]}')
