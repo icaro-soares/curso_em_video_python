f280d2e9d98aee9e04645ebeb97e0072298ab85f
Ícaro Soares <icarosa21@gmail.com>
2026-07-07 09:22:27 -0300

    Atualizei o ex077

--- mundo 3/ex077.py
+++ mundo 3/ex077.py
@@ -1,3 +1,9 @@
 palavras = (
         'ama', 'professor', 'kiwi', 'bolota', 'texto', 'amizade', 'perfeito', 'python', 'guanabara', 'familia',
 )
+for p in palavras:
+    print(f'Vogais em {p.upper()}: ', end='')
+    for vogal in p.lower():
+        if vogal in 'aeiou':
+            print(vogal, end=' ')
+    print()
