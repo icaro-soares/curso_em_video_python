ba6a49a5a5a4364253a344949ccb1f14a57effcb
Ícaro Soares <icarosa21@gmail.com>
2026-07-07 09:45:50 -0300

    Atualizei o ex078

--- mundo 3/ex078.py
+++ mundo 3/ex078.py
@@ -1,7 +1,9 @@
 lista = []
 for c in range(0, 5):
-    lista.append(int(input(f'Digite o {c+1}º valor: ')))
+    lista.append(int(input(f'Digite um valor para a posição {c+1}: ')))
 print('-=' * 30)
 print('Sua lista de números: ', end='')
 for n in lista:
     print(n, end=' ')
+print(f'\nO maior valor é {max(lista)}\nE o menor valor é {min(lista)}.')
+print(f'Suas posições são respectivamente: {lista.index(max(lista))+1} e {lista.index(min(lista))+1}')
