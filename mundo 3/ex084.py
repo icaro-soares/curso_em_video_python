9af7f12e549ea835263360dbff92f5de8b8ae424
Ícaro Soares <icarosa21@gmail.com>
2026-07-13 13:53:36 -0300

    Atualizei o ex084

--- mundo 3/ex084.py
+++ mundo 3/ex084.py
@@ -1,9 +1,13 @@
 dados = []
 temp = []
+cont = pesada = 0
 while True:
     temp.append(input('Nome: ').strip().title())
     temp.append(int(input('Peso (kg): ')))
+    cont += 1
     dados.append(temp[:])
+    if cont == 1 or temp[1] > pesada:
+        pesada = temp[1]
     temp.clear()
     resp = ' '
     while resp not in 'SsNn':
@@ -12,3 +16,4 @@ while True:
         break
 print('-=' * 30)
 print(f'A) Foram cadas cadastradas {len(dados)} pessoas.')
+print(f'B) O maior peso foi de {pesada}kg.')
