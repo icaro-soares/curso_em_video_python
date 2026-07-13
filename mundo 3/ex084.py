4dee846a2d813c0935958375636e43c0298738e9
Ícaro Soares <icarosa21@gmail.com>
2026-07-13 13:46:49 -0300

    Atualizei o ex084

--- mundo 3/ex084.py
+++ mundo 3/ex084.py
@@ -1,20 +1,14 @@
-import os
-from time import sleep
-
-
-lista = []
+dados = []
 temp = []
-c = 0
 while True:
-    temp.append(input('Nome: ').strip())
-    temp.append(int(input('Peso: ')))
-    lista.append(temp[:])
-    c += 1
+    temp.append(input('Nome: ').strip().title())
+    temp.append(int(input('Peso (kg): ')))
+    dados.append(temp[:])
     temp.clear()
-    r = ' '
-    while r not in 'SsNn':
-        r = input('Continuar? [S/N] ')
-    if r in 'Nn':
+    resp = ' '
+    while resp not in 'SsNn':
+        resp = input('Continuar? [S/N] ').strip()[0]
+    if resp in 'Nn':
         break
 print('-=' * 30)
-print(f'Foram cadastradas {len(lista)} pessoas.')
+print(f'A) Foram cadas cadastradas {len(dados)} pessoas.')
