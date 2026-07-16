eeb649785e6d036fce1d5ef14c5753e573d51133
Ícaro Soares <icarosa21@gmail.com>
2026-07-16 09:27:40 -0300

    Atualizei o ex088

--- mundo 3/ex088.py
+++ mundo 3/ex088.py
@@ -6,12 +6,11 @@ temp = []
 qnt = int(input('Quantos jogos devo criar? '))
 for cont in range(0, qnt):
     while len(temp) < 6:
-        n = randint(0, 60)
-        if n not in jogos_mega_sena:
+        n = randint(1, 60)
+        if n not in temp:
             temp.append(n)
     jogos_mega_sena.append(temp[:])
     temp.clear()
-    cont += 1
 print('-=' * 30)
 for i, jogo in enumerate(jogos_mega_sena):
     sleep(1)
