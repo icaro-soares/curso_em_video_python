9f2be0d25f2ffa253a4fd6be77cf04aee2f41c46
Ícaro Soares <icarosa21@gmail.com>
2026-07-16 09:11:00 -0300

    Atualizei o ex088

--- mundo 3/ex088.py
+++ mundo 3/ex088.py
@@ -1,2 +1,11 @@
 from random import randint
 from time import sleep
+
+jogos_mega_sena = []
+while len(jogos_mega_sena) < 6:
+    n = randint(0, 60)
+    if n not in jogos_mega_sena:
+        jogos_mega_sena.append(n)
+print('-=' * 30)
+jogos_mega_sena.sort()
+print(f'Seu jogo: {jogos_mega_sena}')
