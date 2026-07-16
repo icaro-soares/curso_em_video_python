dec0b4f786a4c3aaefc1e05e03bfd5de195658d1
Ícaro Soares <icarosa21@gmail.com>
2026-07-16 19:05:19 -0300

    Atualizei o repositório

---
+++ mundo 3/ex089b.py
@@ -0,0 +1,8 @@
+boletim = {}
+alunos = []
+while True:
+    boletim['nome'] = input('Nome: ')
+    boletim['n1'] = float(input('Nota 1: '))
+    boletim['n2'] = float(input('Nota 2: '))
+    boletim['m'] = (boletim['n1']+boletim['n2'])/2
+    
