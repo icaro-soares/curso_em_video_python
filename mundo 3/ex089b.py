e0a85c06be33d8ac6a57725dee86b5df3ae2e3e8
Ícaro Soares <icarosa21@gmail.com>
2026-07-17 10:30:42 -0300

    Atualizei o ex089b

--- mundo 3/ex089b.py
+++ mundo 3/ex089b.py
@@ -1,8 +1,16 @@
-boletim = {}
-alunos = []
+boletim = []
+alunos = {}
 while True:
-    boletim['nome'] = input('Nome: ')
-    boletim['n1'] = float(input('Nota 1: '))
-    boletim['n2'] = float(input('Nota 2: '))
-    boletim['m'] = (boletim['n1']+boletim['n2'])/2
-    
+    alunos['nome'] = input('Nome: ').strip()
+    alunos['nota1'] = float(input('Nota 1: '))
+    alunos['nota2'] = float(input('Nota 2: '))
+    alunos['média'] = (alunos['nota1'] + alunos['nota2'])/2
+    boletim.append(alunos.copy())
+    r = ' '
+    while r not in 'SsNn':
+        r = input('Quer continuar? [S/N] ').strip()[0]
+    if r in 'Nn':
+        break
+print('-=' * 30)
+for i, v in enumerate(boletim):
+    print(f'')
