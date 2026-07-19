166617883585b6e2f444893f1d370dc4094a4c2e
Ícaro Soares <icarosa21@gmail.com>
2026-07-19 08:32:40 -0300

    Atualizei o repositório

---
+++ mundo 3/ex089b.py
@@ -0,0 +1,32 @@
+aluno = {}
+aluno_temp = {}
+while True:
+    aluno_temp['nome'] = input('Nome: ').strip().title()
+    aluno_temp['nota1'] = float(input('Nota 1: '))
+    aluno_temp['nota2'] = float(input('Nota 2: '))
+    aluno_temp['média'] = (aluno_temp['nota1'] + aluno_temp['nota2'])/2
+    aluno[aluno_temp['nome'], aluno_temp['nota1'], aluno_temp['nota2'], aluno_temp['média']] = aluno_temp.copy()
+    resp = ' '
+    while resp not in 'SsNn':
+        resp = input('Quer continuar? [S/N] ').strip()[0]
+    if resp in 'Nn':
+        break
+print('-=' * 30)
+for k, v in aluno.items():
+    print(f'O campo {k} tem valor {v}')
+
+'''print('-=' * 30)
+print('boletim'.upper().center(30))
+print('-' * 30)
+for i, a in enumerate(aluno):
+    print(f'{i:<10}{a[0]:^10}{a[2]:>10.1f}')
+print('-' * 30)
+while True:
+    n = int(input('Qual aluno devo mostrar as notas? [999 p/ parar] '))
+    if n != 999:
+        print(f'Mostrando as notas de {aluno[n][0]} {aluno[n][1]}')
+    elif n <= len(aluno) - 1:
+        print('Aluno não encontrado!')
+    else:
+        print('Obrigado!')
+        break'''
