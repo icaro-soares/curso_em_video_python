bda537bb5f2f6b8bc254bb5e0e4db89f30849ece
Ícaro Soares <icarosa21@gmail.com>
2026-07-22 09:03:16 -0300

    Atualizei o ex100

--- mundo 3/ex100.py
+++ mundo 3/ex100.py
@@ -3,7 +3,8 @@ from random import randint
 
 def sorteia(lista):
     for c in range(0, 5):
-        lista.append(randint(1, 10))
+        if c not in lista:
+            lista.append(randint(1, 10))
 
 
 def somaPar(lista):
