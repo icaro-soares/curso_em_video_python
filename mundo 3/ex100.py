0c9cbf74c2e967f40c48511cd5c9060294be1f8f
Ícaro Soares <icarosa21@gmail.com>
2026-07-22 09:00:47 -0300

    Atualizei o repositório

--- mundo 3/ex100.py
+++ mundo 3/ex100.py
@@ -6,7 +6,18 @@ def sorteia(lista):
         lista.append(randint(1, 10))
 
 
+def somaPar(lista):
+    s = 0
+    for num in lista:
+        if num % 2 == 0:
+            print(num, end=' ')
+            s += num
+    print(f'\nA soma é {s}')
+
+
 pilha = []
 sorteia(pilha)
 pilha.sort()
 print(f'Números sorteados: {pilha}', )
+print('Os números pares foram: ', end='')
+somaPar(pilha)
