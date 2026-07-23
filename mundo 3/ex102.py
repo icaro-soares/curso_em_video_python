4a17c6ba58d5ab8d680109de722c8a5f78aa8690
Ícaro Soares <icarosa21@gmail.com>
2026-07-23 09:00:19 -0300

    Atualizei o repositório

---
+++ mundo 3/ex102.py
@@ -0,0 +1,17 @@
+def fatorial(n, show=True):
+    m = 1
+    for c in range(n, 0, -1):
+        if show:
+            print(c, end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
+        m *= c
+    return m
+
+
+n = int(input('Digite um valor: '))
+print(f'Mostrando {n}!: ', end='')
+fat = fatorial(n)
+print(fat)
