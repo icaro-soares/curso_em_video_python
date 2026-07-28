016867e6334d9d9082d04dbce1b3cc5282e2bc11
Ícaro Soares <icarosa21@gmail.com>
2026-07-28 10:18:05 -0300

    Atualizei o repositório

---
+++ mundo 3/ex106.py
@@ -0,0 +1,11 @@
+c = (
+        '\033[m', # fundo branco
+        '\033[0; 30; 41m', # fundo vermelho
+        '\033[0; 30; 42m', # fundo verde
+        '\033[0; 30; 43m', # fundo amarelo
+        '\033[0; 30; 44m', # fundo azul
+        '\033[0; 30; 45m', # fundo violeta
+        '\033[0; 30; 46m', # fundo azul anil
+        '\033[0; 30; 47m', # fundo cinza
+        '\033[7; 30m' # inversão de cores
+)
