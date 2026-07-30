de3eaa0e71f256b05cae32299aa564f4215619bf
Ícaro Soares <icarosa21@gmail.com>
2026-07-30 13:51:19 -0300

    Atualizei o repositório

--- mundo 3/ex106.py
+++ mundo 3/ex106.py
@@ -1,20 +1,31 @@
 cores = (
-        '\033[m', # fundo branco
-        '\033[0; 30; 41m', # fundo vermelho
-        '\033[0; 30; 42m', # fundo verde
-        '\033[0; 30; 43m', # fundo amarelo
-        '\033[0; 30; 44m', # fundo azul
-        '\033[0; 30; 45m', # fundo violeta
-        '\033[0; 30; 46m', # fundo azul anil
-        '\033[0; 30; 47m', # fundo cinza
-        '\033[7; 30m' # inversão de cores
+        '\033[m',
+        '\033[0;30;41m',
+        '\033[0;30;42m',
+        '\033[0;30;43m',
+        '\033[0;30;44m',
+        '\033[0;30;45m',
+        '\033[0;30;46m',
+        '\033[0;30;47m',
+        '\033[7;30m]',
 )
 
-def titulo(msg, cor=0):
+def escreva(msg, cor=4):
     tam = len(msg)
-
+    print(cores[cor], end='')
     print('=' * (tam + 4))
     print(f'{msg.center(tam+4)}')
     print('=' * (tam + 4))
+    print(cores[0], end='')
 
+escreva('GUIA DE BIBLIOTECAS PYTHON')
 
+while True:
+    opc = input('Função ou biblioteca > ')
+    if opc == 'fim':
+        escreva('Obrigado!', cor=-3)
+        break
+    if opc in dir(__builtins__):
+        print(help(opc))
+    else:
+        escreva('ERRO: O comando não existe!')
