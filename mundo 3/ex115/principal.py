8202679fe3d44361e58143e66312974e3d3b12a5
Ícaro Soares <icarosa21@gmail.com>
2026-08-04 17:40:18 -0300

    Atualizei o repositório

--- mundo 3/ex115/principal.py
+++ mundo 3/ex115/principal.py
@@ -1,4 +1,4 @@
-from modulos import interface
+from modulos import interface, arquivo
 
 
 while True:
@@ -6,7 +6,14 @@ while True:
     print('-' * 60)
     opc = int(input('Sua opção: '))
     print('-' * 60)
-    if opc == 2:
+    if opc == 1:
+        interface.linha('Mostrando pessoas cadastradas...')
+        if arquivo.arquivoExiste('cursoemvideo.txt'):
+            print('Tudo ok!')
+        else:
+            print('Criando arquivo...')
+            arquivo.criarArquivo('cursoemvideo.txt')
+    elif opc == 2:
         nome = input('Nome: ').strip().title()
         while True:
             idade = input('Idade: ').strip()
