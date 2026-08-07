5fcbf52e8fa0123f2409ebeb52e53cd1b7439512
Ícaro Soares <icarosa21@gmail.com>
2026-08-07 09:39:56 -0300

    Atualizei o repositório

--- mundo 3/ex115/modulos/interface/__init__.py
+++ mundo 3/ex115/modulos/interface/__init__.py
@@ -13,7 +13,7 @@ def menu(lista):
     cabecalho('SISTEMA DE CADASTRO')
     c = 1
     for item in lista:
-        print(f'{c} - {item}')
+        print(f'\033[33m{c}\033[m - \033[32m{item}\033[m')
         c += 1
     print(linha())
     opc = leiaInt('Escolha a opção: ')
