c1ff5197688d6cde01faceb30cdbc9c4b1630c8b
Ícaro Soares <icarosa21@gmail.com>
2026-08-08 10:11:19 -0300

    Atualizei o repositório

---
+++ pratica/ex001/conta.py
@@ -0,0 +1,18 @@
+class ContaBancaria:
+    def __init__(self, titular, saldo=0):
+        self.titular = titular
+        self.saldo = saldo
+
+
+    def deposito(self, valor):
+        self.saldo += valor
+        print(f'Depósito de R${valor:.2f}')
+
+
+    def sacar(self, valor):
+        self.saldo -= valor
+        print(f'Saque de R${valor:.2f}')
+
+
+    def extrato(self):
+        print(f'Extrato: R${self.saldo:.2f}')
---
+++ pratica/ex001/main.py
@@ -0,0 +1,9 @@
+from conta import *
+
+
+pessoa = ContaBancaria('Gustavo', 100)
+pessoa.extrato()
+pessoa.deposito(100)
+pessoa.extrato()
+pessoa.sacar(150)
+pessoa.extrato()
